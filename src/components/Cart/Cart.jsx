5046daa9ace1dc7430b74a8b5930016410c3cdb7
aniruddh kadia <aniruddh.kadia@gmail.com>
2026-02-14 04:20:34 +0530

    add loading in checkout button

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,72 +9,74 @@ import { makePaymentRequest } from "../../utils/api";
 import "./Cart.scss";
 
 const Cart = () => {
-    const { cartItems, setShowCart, cartSubTotal } = useContext(Context);
+  const { cartItems, setShowCart, cartSubTotal } = useContext(Context);
+  const [loading, setLoading] = useState(false);
 
-    const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+  const stripePromise = loadStripe(
+    process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY,
+  );
 
-    const handlePayment = async () => {
-        try {
-            const stripe = await stripePromise;
-            const res = await makePaymentRequest.post("/api/orders", {
-                products: cartItems,
-            });
-            await stripe.redirectToCheckout({
-                sessionId: res.data.stripeSession.id,
-            });
-        } catch (err) {
-            console.log(err);
-        }
-    };
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+      const res = await makePaymentRequest.post("/api/orders", {
+        products: cartItems,
+      });
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-
-    return (
-        <div className="cart-panel">
-            <div
-                className="opac-layer"
-                onClick={() => setShowCart(false)}
-            ></div>
-            <div className="cart-content">
-                <div className="cart-header">
-                    <span className="heading">Shopping Cart</span>
-                    <span
-                        className="close-btn"
-                        onClick={() => setShowCart(false)}
-                    >
-                        <MdClose className="close-btn" />
-                        <span className="text">close</span>
-                    </span>
-                </div>
-
-                {!cartItems.length && (
-                    <div className="empty-cart">
-                        <BsCartX />
-                        <span>No products in the cart.</span>
-                        <button className="return-cta" onClick={() => { }}>
-                            RETURN TO SHOP
-                        </button>
-                    </div>
-                )}
-
-                {!!cartItems.length && (
-                    <>
-                        <CartItem />
-                        <div className="cart-footer">
-                            <div className="subtotal">
-                                <span className="text">Subtotal:</span>
-                                <span className="text total">
-                                    &#8377;{cartSubTotal}
-                                </span>
-                            </div>
-                            <div className="button">
-                                <button className="checkout-cta" onClick={handlePayment}>Checkout</button>
-                            </div>
-                        </div>
-                    </>
-                )}
-            </div>
+  return (
+    <div className="cart-panel">
+      <div className="opac-layer" onClick={() => setShowCart(false)}></div>
+      <div className="cart-content">
+        <div className="cart-header">
+          <span className="heading">Shopping Cart</span>
+          <span className="close-btn" onClick={() => setShowCart(false)}>
+            <MdClose className="close-btn" />
+            <span className="text">close</span>
+          </span>
         </div>
-    );
+
+        {!cartItems.length && (
+          <div className="empty-cart">
+            <BsCartX />
+            <span>No products in the cart.</span>
+            <button className="return-cta" onClick={() => {}}>
+              RETURN TO SHOP
+            </button>
+          </div>
+        )}
+
+        {!!cartItems.length && (
+          <>
+            <CartItem />
+            <div className="cart-footer">
+              <div className="subtotal">
+                <span className="text">Subtotal:</span>
+                <span className="text total">&#8377;{cartSubTotal}</span>
+              </div>
+              <div className="button">
+                <button
+                  className="checkout-cta"
+                  onClick={handlePayment}
+                  disabled={loading}
+                >
+                  {loading ? "Processing..." : "Checkout"}
+                </button>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
