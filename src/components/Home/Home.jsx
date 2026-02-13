8f32b7cb49023191731c40687b5f2478b61f7daa
aniruddh kadia <aniruddh.kadia@gmail.com>
2026-02-14 00:38:00 +0530

    vercel deployment issue fixed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,39 +6,39 @@ import Products from "../Products/Products";
 import { fetchDataFromApi } from "../../utils/api";
 import { Context } from "../../utils/context";
 
-
 const Home = () => {
-    const { categories, setCategories, products, setProducts } = useContext(Context);
-    useEffect(() => {
-        getProducts();
-        getCategories();
-    }, []);
+  const { categories, setCategories, products, setProducts } =
+    useContext(Context);
+  useEffect(() => {
+    getProducts();
+    getCategories();
+  }, [getCategories, getProducts]);
 
-    const getProducts = () => {
-        fetchDataFromApi("/api/products?populate=*").then((res) => {
-            console.log(res)
-            setProducts(res);
-        });
-    };
+  const getProducts = () => {
+    fetchDataFromApi("/api/products?populate=*").then((res) => {
+      console.log(res);
+      setProducts(res);
+    });
+  };
 
-    const getCategories = () => {
-        fetchDataFromApi("/api/categories?populate=*").then((res) => {
-            console.log(res)
-            setCategories(res);
-        });
-    };
+  const getCategories = () => {
+    fetchDataFromApi("/api/categories?populate=*").then((res) => {
+      console.log(res);
+      setCategories(res);
+    });
+  };
 
-    return (
-        <div>
-            <Banner />
-            <div className="main-content">
-                <div className="layout">
-                    <Category categories={categories} />
-                    <Products headingText="Popular Products" products={products} />
-                </div>
-            </div>
+  return (
+    <div>
+      <Banner />
+      <div className="main-content">
+        <div className="layout">
+          <Category categories={categories} />
+          <Products headingText="Popular Products" products={products} />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -1,47 +1,68 @@
 import "./Login.scss";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <>
-
-            <div className="form-container">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-5 mx-auto">
-                            <div className="login form ">
-                                <div class="logo mb-3">
-                                    <div className="col-md-12 text-center">
-                                        <h1 className="heading">Login</h1>
-                                    </div>
-                                </div>
-                                <form action="" method="post" name="login">
-                                    <div className="form-group">
-                                        <label for="exampleInputEmail1">Email address</label>
-                                        <input type="email" name="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label for="exampleInputEmail1">Password</label>
-                                        <input type="password" name="password" id="password" className="form-control" aria-describedby="emailHelp" placeholder="Enter Password" />
-                                    </div>
-                                    <div className="form-group">
-                                        <p className="text-center small-font">By continuing, I agree to the <a className="link" href="#">Terms of Use</a> & <a className="link" href="#">Privacy Plicy</a></p>
-                                    </div>
-                                    <div className="col-md-12 text-center ">
-                                        <button type="submit" className="login-btn">Login</button>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <p class="text-center  small-font mt-2">Don't have account? <a className="link" href="#" id="signup">Sign up here</a></p>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <>
+      <div className="form-container">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-5 mx-auto">
+              <div className="login form ">
+                <div class="logo mb-3">
+                  <div className="col-md-12 text-center">
+                    <h1 className="heading">Login</h1>
+                  </div>
                 </div>
+                <form action="" method="post" name="login">
+                  <div className="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input
+                      type="email"
+                      name="email"
+                      className="form-control"
+                      id="email"
+                      aria-describedby="emailHelp"
+                      placeholder="Enter email"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="exampleInputEmail1">Password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      className="form-control"
+                      aria-describedby="emailHelp"
+                      placeholder="Enter Password"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <p className="text-center small-font">
+                      By continuing, I agree to the
+                      <button type="button"> Terms of Use</button>&
+                      <button type="button"> Privacy Plicy</button>
+                    </p>
+                  </div>
+                  <div className="col-md-12 text-center ">
+                    <button type="submit" className="login-btn">
+                      Login
+                    </button>
+                  </div>
+
+                  <div class="form-group">
+                    <p class="text-center  small-font mt-2">
+                      Don't have account?{" "}
+                      <Link to="/signup"> Sign up here</Link>
+                    </p>
+                  </div>
+                </form>
+              </div>
             </div>
-
-        </>
-
-    )
-}
-export default Login
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Login;
