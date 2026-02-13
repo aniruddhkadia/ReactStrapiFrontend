57af67b5f67ab57d56bbab883834bd0d7775c6e0
aniruddh kadia <aniruddh.kadia@gmail.com>
2026-02-14 04:32:41 +0530

    checkout button issue resolve

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { MdClose } from "react-icons/md";
 import { BsCartX } from "react-icons/bs";
 import { Context } from "../../utils/context";
